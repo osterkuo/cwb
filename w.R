2ae0346a5de7c6b046d3e5318faf36f83a964f1d
oster <ethan355223@gmail.com>
2017-12-19 20:18:52 +0800

    Signed-off-by: oster <ethan355223@gmail.com>

--- w.R
+++ w.R
@@ -18,11 +18,16 @@ aug = getweather('201708.csv', '201708')
 sep = getweather('201709.csv', '201709')
 oct = getweather('201710.csv', '201710')
 nov = getweather('201711.csv', '201711')
+dec = getweather('201712.csv', '201712')
 par(mar=c(5,4,4,2)+1)
 boxplot(sep[,c(3,4,5)],ylab="放" ,xlab="201709", las=1)
 
 boxplot(aug[,c(3,4,5)],ylab="放" ,xlab="201708", las=1)
 boxplot(nov[,c(3,4,5)],ylab="放" ,xlab="201711", las=1)
 
-plot(jul$代溃,type="b", pch=16, col="black",las=1,xlab="ら戳",
-     ylab="溃",main="2017/07", axes=TRUE)
+plot(dec$代溃,type="b", pch=16, col="black",las=1,xlab="ら戳",
+     ylab="溃",main="2017/12", axes=TRUE)
+boxplot(des[,c(3,4,5)],ylab="放" ,xlab="201712", las=1)
+w2017=rbind(jul,aug,sep,oct,nov,dec)
+table(w2017$label)
+plot(放~2017,las=1)
